2da482eb1bedb18debe5c44098ff728623a53132
Ryan Dranginis <ryan.dranginis@gmail.com>
2015-03-30 09:24:03 -0700

    added shousers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'pages/home'
+  get "pages/showusers"
   root "signups#new"
 
   # The priority is based upon order of creation: first created -> highest priority.
